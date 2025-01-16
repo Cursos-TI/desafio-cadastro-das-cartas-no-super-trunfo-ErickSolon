6334d69572b5f67d80988b21841c4d0d067a125c
Érick <72041638+ErickSolon@users.noreply.github.com>
2025-01-16 22:45:57 +0000

    implementação de lógica para cálculo de PIB per capita e densidade populacional

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-
 #define NUM_ESTADOS 8
 #define CIDADES_POR_ESTADO 4
 #define TOTAL_CIDADES (NUM_ESTADOS * CIDADES_POR_ESTADO)
 
+float calcularDensidadePopulacional(unsigned int populacao, float area) {
+    if (area > 0) {
+        return populacao / area;
+    } else {
+        return 0;
+    }
+}
+
+float calcularPibPerCapita(float pib, unsigned int populacao) {
+    if (populacao > 0) {
+        return pib / populacao;
+    } else {
+        return 0;
+    }
+}
+
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     char codigoCidade[TOTAL_CIDADES][4];
     char nome[TOTAL_CIDADES][100]; 
     unsigned int populacao[TOTAL_CIDADES];
@@ -20,10 +29,9 @@ int main() {
     float pib[TOTAL_CIDADES]; 
     unsigned int pontosTuristicos[TOTAL_CIDADES];
     int indice = 0;
+    double pibPerCapita;
+    double densidadePopulacional;
 
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     for (char estado = 'A'; estado <= 'H'; estado++) {
         for (int cidade = 1; cidade <= 4; cidade++) {
             sprintf(codigoCidade[indice], "%c%02d", estado, cidade);
@@ -45,14 +53,19 @@ int main() {
         }
     }
 
-    printf("\n=== Cartas das Cidades ===\n");
+    printf("\n=== Cartas das cidades ===\n");
     for (int i = 0; i < TOTAL_CIDADES; i++) {
-        printf("\n=== Código da Cidade %s ===\n", codigoCidade[i]);
+        densidadePopulacional = calcularDensidadePopulacional(populacao[i], area[i]);
+        pibPerCapita = calcularPibPerCapita(pib[i], populacao[i]);
+
+        printf("\n=== Código da cidade %s ===\n", codigoCidade[i]);
         printf("Nome da cidade: %s\n", nome[i]);
         printf("População da cidade: %u\n", populacao[i]);
         printf("Área da cidade: %.2f km²\n", area[i]);
         printf("PIB da cidade: %.2f bilhões\n", pib[i]);
         printf("Número de pontos turísticos: %u\n", pontosTuristicos[i]);
+        printf("Densidade populacional: %.2f hab/km²\n", densidadePopulacional);
+        printf("PIB per capita: %.2f bilhões/habitante\n", pibPerCapita);
     }
 
 
